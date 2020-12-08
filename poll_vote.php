bc6d6cb717dc4177609bf4bb008b3ed2f19aad57
chhvap <73684298+chhvap@users.noreply.github.com>
2020-12-08 21:54:38 +0530

    Update poll_vote.php

--- poll_vote.php
+++ poll_vote.php
@@ -11,14 +11,14 @@ $yes = $array[0];
 $no = $array[1];
 
 if ($vote == 0) {
-  $yes = $yes + 1;
+  $abhijit = $abhijit + 1;
 }
 if ($vote == 1) {
-  $no = $no + 1;
+  $monal = $monal + 1;
 }
 
 //insert votes to txt file
-$insertvote = $yes."||".$no;
+$insertvote = $abhijit."||".$monal;
 $fp = fopen($filename,"w");
 fputs($fp,$insertvote);
 fclose($fp);
@@ -29,17 +29,17 @@ fclose($fp);
 <tr>
 <td>Yes:</td>
 <td><img src="poll.gif"
-width='<?php echo(100*round($yes/($no+$yes),2)); ?>'
+width='<?php echo(100*round($abhijit/($monal+$abhijit),2)); ?>'
 height='20'>
-<?php echo(100*round($yes/($no+$yes),2)); ?>%
+<?php echo(100*round($abhijit/($monal+$abhijit),2)); ?>%
 </td>
 </tr>
 <tr>
 <td>No:</td>
 <td><img src="poll.gif"
-width='<?php echo(100*round($no/($no+$yes),2)); ?>'
+width='<?php echo(100*round($abhijit/($monal+$abhijit),2)); ?>'
 height='20'>
-<?php echo(100*round($no/($no+$yes),2)); ?>%
+<?php echo(100*round($abhijit/($monal+$abhijit),2)); ?>%
 </td>
 </tr>
 </table>
